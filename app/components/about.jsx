1844db1e3a7a521605c4a271e0c3a68f47eefff8
Ahmad <ahmadbratton@yahoo.com>
2017-10-29 05:38:33 -0400

    added links to about

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -2,10 +2,22 @@ var React = require("react");
 
 
 
-
 var about = (props) => {
   return(
-    <h3>about components</h3>
+    <div>
+      <h1 className="text-center" > About </h1>
+      <p> weather that built while learning react </p>
+      <p>here are some of the tools I used</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react" > react </a> - this was the javaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org"> open weather map </a> I used open weather
+          map to search for city name 
+        </li>
+      </ul>
+    </div>
   )
 }
 
--- app/components/main.jsx
+++ app/components/main.jsx
@@ -1,11 +1,11 @@
 var React = require('react');
 var Nav = require("app/components/nav.jsx");
-const hi = 'hi';
+
 
 var main = (props) => {
   return(
     <div>
-      <Nav title='hi' />
+      <Nav/>
       <div className="row" >
         <div className="columns medium-6 large-4 small-centered " >
           {props.children}
